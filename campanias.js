4ba5f19f04ec068230cf213f11ed3985954fc2c6
El Shopper <elshopper@gmail.com>
2026-07-16 12:59:44 -0500

    Agrega campaña Shopper Digital

--- campanias.js
+++ campanias.js
@@ -1,30 +1,38 @@
 const CAMPANIAS = {
+
   "tiktok-kuya": {
     cliente: "kuya",
     campania: "catalogo_julio",
     origen: "tiktok",
     destino: "https://www.tiktok.com/@joyeriakuyaperu"
-},
+  },
 
   "facebook-kuya": {
     cliente: "kuya",
     campania: "catalogo_julio",
     origen: "facebook",
     destino: "https://www.facebook.com/perukuya/"
-},
+  },
 
-"youtube-kuya": {
+  "youtube-kuya": {
     cliente: "kuya",
     campania: "catalogo_julio",
     origen: "youtube",
     destino: "https://www.youtube.com/@KuyaPeruSilver"
-},
+  },
 
-"instagram-kuya": {
+  "instagram-kuya": {
     cliente: "kuya",
     campania: "catalogo_julio",
     origen: "instagram",
     destino: "https://www.instagram.com/kuyaperujoyeria/"
-},
+  },
+
+  "meta-shopper-app": {
+    cliente: "shopper",
+    campania: "lanzamiento_app",
+    origen: "meta",
+    destino: "https://elshopper-pwa.vercel.app/"
+  }
 
 };
