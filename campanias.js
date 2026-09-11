6dc1bcc92c42243b97476beb11bea838f6311a79
El Shopper <elshopper@gmail.com>
2026-09-11 10:36:10 -0500

    Agrega campañas Meta Shopper 1 2 y 3

--- campanias.js
+++ campanias.js
@@ -5,6 +5,27 @@ const CAMPANIAS = {
     campania: "kuya_tiktok_whatsapp_sep26",
     origen: "tiktok",
     destino: "https://wa.me/51902291005"
+  },
+
+  "meta-shopper-campania-1": {
+    cliente: "shopper",
+    campania: "shopper_meta_campania_1",
+    origen: "meta",
+    destino: "https://elshopper-pwa.vercel.app/"
+  },
+
+  "meta-shopper-campania-2": {
+    cliente: "shopper",
+    campania: "shopper_meta_campania_2",
+    origen: "meta",
+    destino: "https://elshopper-pwa.vercel.app/"
+  },
+
+  "meta-shopper-campania-3": {
+    cliente: "shopper",
+    campania: "shopper_meta_campania_3",
+    origen: "meta",
+    destino: "https://elshopper-pwa.vercel.app/"
   }
 
 };
